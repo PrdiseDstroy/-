ad200b721bf4ad56cd7133a26549ee3ae752bff3
PrdiseDstroy <iv.pavli99k@gmail.com>
2024-11-10 16:58:14 +0300

    cycle While.py

--- cycle While.py
+++ cycle While.py
@@ -1,43 +1,43 @@
 while True:
     my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
     my_list.pop(4)
-    if my_list[0]>0:
+    if my_list[0] > 0:
         print(my_list[0])
     else:
         break
-    if my_list[6]>0:
+    if my_list[6] > 0:
         print(my_list[1])
     else:
         break
-    if my_list[2]>0:
+    if my_list[2] > 0:
         print(my_list[2])
     else:
         break
-    if my_list[3]>0:
+    if my_list[3] > 0:
         print(my_list[3])
     else:
         break
-    if my_list[4]>0:
+    if my_list[4] > 0:
         print(my_list[4])
     else:
         break
-    if my_list[5]>0:
+    if my_list[5] > 0:
         print(my_list[5])
     else:
         break
-    if my_list[6]>0:
+    if my_list[6] > 0:
         print(my_list[6])
     else:
         break
-    if my_list[7]>0:
+    if my_list[7] > 0:
         print(my_list[7])
     else:
         break
-    if my_list[8]>0:
+    if my_list[8] > 0:
         print(my_list[8])
     else:
         break
-    if my_list[9]>0:
+    if my_list[9] > 0:
         print(my_list[9])
     else:
         break
