6aedeef53320aa89d56bc1d163e71968b3f9d08a
PrdiseDstroy <iv.pavli99k@gmail.com>
2024-11-29 17:26:49 +0300

    Add files via upload

---
+++ practical_task_3_modul'.py
@@ -0,0 +1,24 @@
+data_structure = [
+ [1, 2, 3],
+ {'a': 4, 'b': 5},
+ (6, {'cube': 7, 'drum': 8}),
+ "Hello",
+ ((), [{(2, 'Urban', ('Urban2', 35))}])]
+
+def calculate_structure_sum(data_structure):
+    summa = 0
+    if isinstance(data_structure, int):
+        summa += data_structure
+    elif isinstance(data_structure, str):
+        summa += len(data_structure)
+    elif isinstance(data_structure, dict):
+        for i, j in data_structure.items():
+            summa += calculate_structure_sum(i)
+            summa += calculate_structure_sum(j)
+    elif isinstance(data_structure, (list, tuple, set)):
+        for k in data_structure:
+            summa += calculate_structure_sum(k)
+    return summa
+
+result = calculate_structure_sum(data_structure)
+print(result)
