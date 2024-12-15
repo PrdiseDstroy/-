9f3f3d86166650ac5bf9542e816761c1245bf9de
PrdiseDstroy <iv.pavli99k@gmail.com>
2024-12-15 15:47:11 +0300

    Add files via upload

--- Namespace.py
+++ Namespace.py
@@ -1,19 +1,5 @@
-calls = 0
-def count_calls():
-    global calls
-    calls += 1
-    return calls
-def string_info(string):
-    count_calls()
-    global calls
-    b = (len(string), string.upper(), string.lower())
-    return b
-def is_contains(string, list_to_search ):
-    count_calls()
-    a = string.lower() in list_to_search
-    return a
-
-print(string_info('Hello'))
-print(is_contains('one'.lower(), ['one', 'two', 'three']))
-print((is_contains('apple', ['one', 'two', 'three'])))
-print(calls)
+def test_function(a):
+    def inner_function(a):
+        return print('Я в области видимости функции test_function')
+    inner_function(a)
+test_function('')
