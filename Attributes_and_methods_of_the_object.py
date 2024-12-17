1645a1a5560dd7b3c6e4497073830106cba796f3
PrdiseDstroy <iv.pavli99k@gmail.com>
2024-12-17 14:18:22 +0300

    Attributes_and_methods_of_the_object.py

--- Attributes_and_methods_of_the_object.py
+++ Attributes_and_methods_of_the_object.py
@@ -2,13 +2,12 @@ class Hous:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-    
     def go_to(self, new_floor):
-        for i in range(self.number_of_floors):
-            if i >= 1 and i <= self.number_of_floors + 1:
-                print(i)
-        else:
-            return ('Такого этажа не существует')
+            if new_floor >= 1 and new_floor <= self.number_of_floors:
+                for i in range(1, new_floor + 1):
+                    print(i)
+            else:
+                print('Такого этажа не существует')
 
-h1 = Hous('ЖК ЖелДор', 30)
-print(h1.go_to(12))
+h1 = Hous('ЖК ЖелДор', 22)
+h1.go_to(13)
